9abe2ff11b78bdd4b73630f355a02a21a11c639b
2staffi@gmail.com <2staffi@gmail.com>
2017-09-06 01:07:07 -0400

    Test for only numbers in isbn string

--- test.groovy
+++ test.groovy
@@ -23,11 +23,16 @@ class IsbnTest extends GroovyTestCase {
     void testRemoveDashTen() {
        def dashes = new Isbn(isbn : '0-471-95869-7')
        assertToString(dashes.RemoveSpacesAndDashes(), '0471958697')
-   }
+    }
 
             void testRemoveDashThirt() {
                 def dashes = new Isbn(isbn : '9-780-4700-5902-9')
                 assertToString(dashes.RemoveSpacesAndDashes(), '9780470059029')
             }
+
+    void testStringOnlyNumbers() {
+        def numOnly = new Isbn(isbn : '123524875')
+        assertToString(numOnly.OnlyNumbers(), 'true')
+    }
 
 }
