cdda2510bc182859fb69cf4c52224a89643b2264
2staffi@gmail.com <2staffi@gmail.com>
2017-09-10 16:46:17 -0400

    math checks out and results check out but isbn 13 keeps returning false

--- ISBN.groovy
+++ ISBN.groovy
@@ -42,4 +42,43 @@ public class Isbn {
         }
     }
 
+    def ValidThirteenIsbn() {
+        def sum = 0
+        println "this is sum at first - $sum"
+        String[] isbnArray
+        println "this is the start of isbnArray - $isbnArray"
+        isbnArray = isbn
+        println "this is isbnArray with inbn in it - $isbnArray"
+        isbnArray.eachWithIndex { number, index ->
+            if (index <= 11) {
+                if (index % 2 == 0) {
+                println "this is the current index mod 2 - $index"
+                println "this is the current number mod 2 - $number"
+                    sum += number.toInteger() * 1
+                    println "sum - $sum"
+                } else {
+                println "this is the current index - $index"
+                println "this is the current number - $number"
+                    sum += number.toInteger() * 3
+                    println "sum - $sum"
+                }
+            }
+        }
+        println "this is sum at the end of it all - $sum"
+        int pre_pre_check = sum % 10
+        println "pre pre check should be 0 - $pre_pre_check"
+        int pre_check = 10 - pre_pre_check
+        println "pre check should be 10 - $pre_check"
+        int check_sum = pre_check % 10
+        println "check sum should be 0 - $check_sum"
+        String test = isbnArray[12]
+            println "isbnArray index 12 should be 0 - $test and check sum should be 0 - $check_sum"
+        if (check_sum == isbnArray[12]) {
+            return true
+        } else {
+            return false
+        }
+
+    }
+
 }
